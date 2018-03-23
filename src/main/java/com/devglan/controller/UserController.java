b2c38e389c509a04ac55345db4e411ebae00b368
sagar <thakkarsagar12@gmail.com>
2018-03-23 22:56:28 +0530

    final

--- src/main/java/com/devglan/controller/UserController.java
+++ src/main/java/com/devglan/controller/UserController.java
@@ -7,19 +7,20 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+
 @CrossOrigin("http://localhost:4200")
+@RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value="/user", method = RequestMethod.GET)
+    @RequestMapping(value="/users", method = RequestMethod.GET)
     public List<User> listUser(){
         return userService.findAll();
     }
 
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
     public User getOne(@PathVariable(value = "id") Long id){
         return userService.findById(id);
     }
